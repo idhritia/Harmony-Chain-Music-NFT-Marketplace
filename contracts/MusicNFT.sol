4cf0a7644436b1a764ace5baa0c573533a9a38dd
Dhriti Ankapalli <dhritia04@gmail.com>
2025-04-24 15:17:17 +0530

    Update MusicNFT.sol

--- contracts/MusicNFT.sol
+++ contracts/MusicNFT.sol
@@ -1,133 +1,91 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MusicNFT is ERC721URIStorage, Ownable {
+/**
+ * @title MusicNFT
+ * @dev Contract for minting and managing music NFTs with royalty information
+ */
+contract MusicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     
-    // Structure to store music metadata
-    struct Music {
-        string title;
-        string artist;
-        string genre;
-        uint256 price;
-        address payable creator;
-        bool isForSale;
+    // Struct to store music metadata and royalty info
+    struct MusicMetadata {
+        string audioURI;        // URI to the audio file (MP3)
+        string coverImageURI;   // URI to the cover image
+        address creator;        // Original creator address
+        uint256 royaltyPercent; // Royalty percentage (out of 100)
     }
     
-    // Mapping from token ID to Music
-    mapping(uint256 => Music) public musicData;
+    // Maps token ID to its metadata
+    mapping(uint256 => MusicMetadata) public musicData;
     
     // Events
-    event MusicMinted(uint256 tokenId, string title, string artist, address creator);
-    event MusicPriceChanged(uint256 tokenId, uint256 newPrice);
-    event MusicSold(uint256 tokenId, address from, address to, uint256 price);
+    event MusicNFTMinted(uint256 tokenId, address creator, string audioURI, string coverImageURI);
     
-    constructor() ERC721("HarmonyChain", "HCNFT") {}
+    constructor() ERC721("HarmonyChainMusic", "HCM") {}
     
-    // Mint new music NFT
-    function mintMusic(
-        string memory title,
-        string memory artist,
-        string memory genre,
-        uint256 price,
-        string memory tokenURI
+    /**
+     * @dev Mint a new music NFT
+     * @param recipient The address that will own the NFT
+     * @param tokenURI The IPFS URI for the token metadata
+     * @param audioURI The URI to the audio file (MP3)
+     * @param coverImageURI The URI to the cover image
+     * @param royaltyPercent The percentage of sales that go to the creator as royalty
+     * @return The ID of the newly minted NFT
+     */
+    function mintMusicNFT(
+        address recipient,
+        string memory tokenURI,
+        string memory audioURI,
+        string memory coverImageURI,
+        uint256 royaltyPercent
     ) public returns (uint256) {
-        _tokenIds.increment();
+        require(royaltyPercent <= 30, "Royalty cannot exceed 30%");
         
+        _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
-        _mint(msg.sender, newTokenId);
+        
+        _mint(recipient, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
         
-        musicData[newTokenId] = Music({
-            title: title,
-            artist: artist,
-            genre: genre,
-            price: price,
-            creator: payable(msg.sender),
-            isForSale: true
+        // Store the music-specific metadata
+        musicData[newTokenId] = MusicMetadata({
+            audioURI: audioURI,
+            coverImageURI: coverImageURI,
+            creator: msg.sender,
+            royaltyPercent: royaltyPercent
         });
         
-        emit MusicMinted(newTokenId, title, artist, msg.sender);
+        emit MusicNFTMinted(newTokenId, msg.sender, audioURI, coverImageURI);
         
         return newTokenId;
     }
     
-    // Change music price
-    function changePrice(uint256 tokenId, uint256 newPrice) public {
-        require(_exists(tokenId), "Music NFT does not exist");
-        require(ownerOf(tokenId) == msg.sender, "Only owner can change price");
-        
-        musicData[tokenId].price = newPrice;
-        emit MusicPriceChanged(tokenId, newPrice);
+    /**
+     * @dev Get royalty information for a token
+     * @param tokenId The ID of the token
+     * @return creator The creator address
+     * @return royaltyPercent The royalty percentage
+     */
+    function getRoyaltyInfo(uint256 tokenId) public view returns (address creator, uint256 royaltyPercent) {
+        require(_exists(tokenId), "Token does not exist");
+        MusicMetadata memory metadata = musicData[tokenId];
+        return (metadata.creator, metadata.royaltyPercent);
     }
     
-    // Toggle sale status
-    function toggleForSale(uint256 tokenId) public {
-        require(_exists(tokenId), "Music NFT does not exist");
-        require(ownerOf(tokenId) == msg.sender, "Only owner can toggle sale status");
-        
-        musicData[tokenId].isForSale = !musicData[tokenId].isForSale;
-    }
-    
-    // Buy music NFT
-    function buyMusic(uint256 tokenId) public payable {
-        require(_exists(tokenId), "Music NFT does not exist");
-        require(musicData[tokenId].isForSale, "Music NFT is not for sale");
-        require(msg.value >= musicData[tokenId].price, "Insufficient funds");
-        
-        address seller = ownerOf(tokenId);
-        address buyer = msg.sender;
-        uint256 price = musicData[tokenId].price;
-        
-        // Transfer ownership
-        _transfer(seller, buyer, tokenId);
-        
-        // Pay the seller
-        payable(seller).transfer(price);
-        
-        // Set status to not for sale after purchase
-        musicData[tokenId].isForSale = false;
-        
-        emit MusicSold(tokenId, seller, buyer, price);
-        
-        // Return excess funds
-        if (msg.value > price) {
-            payable(buyer).transfer(msg.value - price);
-        }
-    }
-    
-    // Get total supply of NFTs
-    function getTotalMusicCount() public view returns (uint256) {
-        return _tokenIds.current();
-    }
-    
-    // Get music information
-    function getMusicInfo(uint256 tokenId) public view returns (
-        string memory title,
-        string memory artist,
-        string memory genre,
-        uint256 price,
-        address creator,
-        bool isForSale,
-        address currentOwner
-    ) {
-        require(_exists(tokenId), "Music NFT does not exist");
-        
-        Music memory music = musicData[tokenId];
-        
-        return (
-            music.title,
-            music.artist,
-            music.genre,
-            music.price,
-            music.creator,
-            music.isForSale,
-            ownerOf(tokenId)
-        );
+    /**
+     * @dev Get music content URIs
+     * @param tokenId The ID of the token
+     * @return audioURI The audio file URI
+     * @return coverImageURI The cover image URI
+     */
+    function getMusicContent(uint256 tokenId) public view returns (string memory audioURI, string memory coverImageURI) {
+        require(_exists(tokenId), "Token does not exist");
+        MusicMetadata memory metadata = musicData[tokenId];
+        return (metadata.audioURI, metadata.coverImageURI);
     }
 }
